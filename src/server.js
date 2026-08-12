911f2eaf1c73b1c7d2fe59f560d468dafa7767af
Lucas Oliveira <Lucasp.oliveira822@gmail.com>
2026-08-12 10:29:34 -0300

    refactor: doing some changes

--- src/server.js
+++ src/server.js
@@ -18,6 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API All Books no Ar!")
-  console.log("Disponível em: http://localhost:8000")
+  console.log("API Acessivel em: http://localhost:8000")
 })
