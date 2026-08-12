cc139e28b5e050da50136bc5c35ed6587fc53068
Lucas Oliveira <Lucasp.oliveira822@gmail.com>
2026-08-12 10:42:16 -0300

    refactor: updating new information

--- src/middleware/authenticationMiddleware.js
+++ src/middleware/authenticationMiddleware.js
@@ -4,7 +4,7 @@ const { verifyToken } = require('../services/authService');
 function authenticationMiddleware(req, res, next) {
     if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
         const status = 401;
-        const message = 'Token inválido';
+        const message = 'o token não é válido.';
         res.status(status).json({ status, message });
         return;
     }
